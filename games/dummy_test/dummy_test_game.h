bd4203ef8fbe4c044c41043a57b50c3363c52289
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-07-20 00:14:21 +0200

    some fiddling

--- games/dummy_test/dummy_test_game.h
+++ games/dummy_test/dummy_test_game.h
@@ -4,9 +4,12 @@
 #include "state_menu.h"
 #include "state_play.h"
 #include "../../src/ufo/game.h"
+#include "../../external/olcPixelGameEngine.h"
 
 class DummyTestGame : public Game{
 public:
+    const olc::Pixel SEMI_SOLID_COLOUR = olc::Pixel(255, 0, 255, 255);
+    const olc::Pixel SOLID_COLOUR = olc::Pixel(255, 255, 255, 255);
     StateLoad *state_load;
     StateMenu *state_menu;
     StatePlay *state_play;
