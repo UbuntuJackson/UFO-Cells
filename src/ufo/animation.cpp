2f144eb0a4b93da9c729bb7635538706591c0c9f
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-09-01 20:24:42 +0200

    new animation class exists at least

--- src/ufo/animation.cpp
+++ src/ufo/animation.cpp
@@ -1,1 +1,47 @@
 #include "animation.h"
+#include "camera.h"
+
+Animation::Animation(){
+    current_anim = idle_stand;
+    Play();
+}
+
+void
+Animation::Play(){
+    is_playing = true;
+}
+
+void
+Animation::Stop(){
+    is_playing = false;
+}
+
+void
+Animation::Update(){
+    if(is_playing) frame_count += delta_frames;
+    UpdateStateLogic();
+}
+
+void
+Animation::ResetAndPlay(){
+    frame_count = 0.0f;
+    Play();
+}
+
+void
+Animation::UpdateStateLogic(){
+    /*
+    Here goes the instructions for when for example an idle animation should start switching to subanimations. For example:
+    if(frame_count > 6000.0f){current_anim = idle_wait;}
+    */
+}
+
+void
+Animation::Draw(Camera* _camera){
+    //partial_decal = Decal(w, h);
+    //sample bunch of pixles according to a sprite index
+    //Draw the created decal with camera
+    //Requires lists with chopped up spritesheets to exist
+
+    //Maybe create a function for camera that takes both a decal and an index
+}
--- src/ufo/animation.h
+++ src/ufo/animation.h
@@ -7,11 +7,19 @@
 class Animation{
 public:
     float delta_frames;
-    std::vector<std::string> frame_decals;
-    std::string anim_directory;
+    float frame_count;
+    bool is_playing;
+    std::string sprite_sheet;
+    std::vector<int> current_anim;
+    std::vector<int> idle_stand; //example, will not remain in this class, but can be added in classes inheriting from this class
+    std::vector<int> idle_wait; //example, will not remain in this class, but can be added in classes inheriting from this class
     Animation() = default;
-    Animation(std::string _directory);
-    void LoadFrames(std::string _directory);
-    void UpdateDraw();
+    Animation(std::string _sprite_sheet);
+    void Play();
+    void Stop();
+    void ResetAndPlay();
+    void UpdateStateLogic();
+    void Update();
+    void Draw(Camera* _camera);
 };
 #endif
