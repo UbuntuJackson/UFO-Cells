2e4d4fff2f8cc8b609ac15fae8c74ce5c1f9ad3e
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-09-21 00:50:44 +0200

    sin wave example

--- src/ufo/dynamic_solid.cpp
+++ src/ufo/dynamic_solid.cpp
@@ -6,17 +6,17 @@
 #include <cmath>
 
 DynamicSolid::DynamicSolid(olc::vf2d _position, Game* _game, std::string _mask) : CellActor(_position, _game, _mask){
-    velocity = {-0.3f, -0.3f};
+    velocity = {0.0f, 5.3f};
     timelapse = 0;
 }
 
 void DynamicSolid::Update(){
     position += velocity;
-    angle+=0.01;
-    velocity.x = 20.0f * -std::cos(angle); //change vel after movement to avoid wacky inaccurate adding of velocity
-    velocity.y = 20.0f * std::sin(angle);
+    angle+=0.1;
+    velocity.x = 2.0f; //change vel after movement to avoid wacky inaccurate adding of velocity
+    velocity.y = 5.0f * std::sin(angle);
     std::cout << velocity.y << std::endl;
-    timelapse++;
+    //timelapse++;
     //if(timelapse > 400){velocity *= -1.0f; timelapse = 0;}
 }
 void DynamicSolid::Draw(Camera* _camera){
