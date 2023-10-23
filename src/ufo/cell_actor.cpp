1b7931587bac1171555c6573f5b6507e0adccbe0
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-10-23 23:21:40 +0200

    changed IsBeingEntered

--- src/ufo/cell_actor.cpp
+++ src/ufo/cell_actor.cpp
@@ -477,7 +477,7 @@ CellActor::AdjustDownSlope(CellMap* _map){
 
 
 bool CellActor::IsBeingEntered(olc::vf2d _position, olc::vf2d _delta_position, int _direction, std::string _mask){
-    if(!IsOverlappingOtherDecal(game->asset_manager.GetDecal(_mask), _position + _delta_position, game->asset_manager.GetDecal(mask), position)){
+    if(!IsOverlappingOtherDecal(game->asset_manager.GetDecal(_mask), _position, game->asset_manager.GetDecal(mask), position)){
         if(IsOverlappingOtherDecal(game->asset_manager.GetDecal(_mask), {_position.x + _delta_position.x, _position.y}, game->asset_manager.GetDecal(mask), position)){
             if(_delta_position.x > 0){
                 return _direction == UfoGlobal::LEFT;
@@ -487,7 +487,7 @@ bool CellActor::IsBeingEntered(olc::vf2d _position, olc::vf2d _delta_position, i
             }
         }
     }
-    if(!IsOverlappingOtherDecal(game->asset_manager.GetDecal(_mask), _position + _delta_position, game->asset_manager.GetDecal(mask), position)){
+    if(!IsOverlappingOtherDecal(game->asset_manager.GetDecal(_mask), {_position.x + _delta_position.x, _position.y}, game->asset_manager.GetDecal(mask), position)){
         if(IsOverlappingOtherDecal(game->asset_manager.GetDecal(_mask), _position + _delta_position, game->asset_manager.GetDecal(mask), position)){
             if(_delta_position.y > 0){
                 return _direction == UfoGlobal::UP;
