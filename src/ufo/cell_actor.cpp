451a8ba2a00c1537c90c7a600fc822c0ad47c739
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-11-07 21:00:26 +0100

    made it velocity.y instead of delta position

--- src/ufo/cell_actor.cpp
+++ src/ufo/cell_actor.cpp
@@ -296,7 +296,7 @@ CellActor::AdjustCollisionY(CellMap* _map){
     //SEMI SOLID
 
     if(IsOverlapping(_map,mask_decal, solid_layer, position, olc::RED) &&
-        position.y-former_position.y > 0.0f &&
+        velocity.y > 0.0f &&
         !is_already_in_semi_solid){
         
         position.y = std::floor(position.y);
