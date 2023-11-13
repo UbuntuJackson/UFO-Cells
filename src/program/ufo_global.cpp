4f0a8cbf7ea5863813431904b0cec504ce7729a3
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-11-13 20:34:34 +0100

    a

--- src/program/ufo_global.cpp
+++ src/program/ufo_global.cpp
@@ -3,7 +3,7 @@
 #include "../scripting_utils/squirrel_interface.h"
 namespace UfoGlobal{
     SquirrelInterface squirrel_interface;
-    const RED = olc::RED;
+    const olc::Pixel RED = olc::RED;
     const olc::Pixel ORANGE = olc::Pixel(255,136,0);
     const olc::Pixel YELLOW = olc::Pixel(255,221,0);
     const olc::Pixel LEMON = olc::Pixel(255, 221, 170);
