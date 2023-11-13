9ab8a2d8a288bd0d62af4dff02275e3f21708e7d
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-11-13 20:45:29 +0100

    a

--- src/ufo/cell_actor.cpp
+++ src/ufo/cell_actor.cpp
@@ -294,19 +294,6 @@ CellActor::AdjustCollisionY(CellMap* _map){
     }
 
     //SEMI SOLID
-
-    if(IsOverlapping(_map,mask_decal, solid_layer, position, olc::RED) &&
-        velocity.y > 0.0f &&
-        !is_already_in_semi_solid){
-        
-        position.y = std::floor(position.y);
-        while(IsOverlapping(_map,mask_decal, solid_layer, position, olc::RED)){
-            position.y -= 1.0f;
-        }
-        
-
-        velocity.y = 0.0f;
-    }
 }
 
 void
