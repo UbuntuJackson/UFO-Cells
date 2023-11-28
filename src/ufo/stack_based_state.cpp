522f873e60fdb216dd530dec08b371f17514487c
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-11-28 12:11:12 +0100

    update status is true

--- src/ufo/stack_based_state.cpp
+++ src/ufo/stack_based_state.cpp
@@ -1,7 +1,10 @@
 #include "stack_based_state.h"
 #include "stack_based_state_machine.h"
 
-StackBasedState::StackBasedState(StackBasedStateMachine* _state_machine) : state_machine{_state_machine}{}
+StackBasedState::StackBasedState(StackBasedStateMachine* _state_machine) :
+    state_machine{_state_machine},
+    update_active{true},
+    draw_active{true}{}
 
 void StackBasedState::Update(){}
 void StackBasedState::Draw(){}
