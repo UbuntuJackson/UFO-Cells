58369322427db24eb060ac116bbf8203e33a6d16
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-11-28 10:53:51 +0100

    forgot to change CellMap to Level in a spot

--- src/ufo/cell_actor.h
+++ src/ufo/cell_actor.h
@@ -50,7 +50,7 @@ public:
     int IsOverlappingHeight(Level* _map, olc::Decal *_decal, std::string _layer, olc::vf2d _position, olc::Pixel _colour = olc::WHITE);
     int HeightUntilGround(Level* _map, olc::Decal *_decal, std::string _layer, olc::vf2d _position, olc::Pixel _colour = olc::WHITE);
     bool IsOverlappingOtherDecal(olc::Decal *_decal, olc::vf2d _position, olc::Decal *_other_decal, olc::vf2d _other_position, olc::Pixel _colour = olc::WHITE);
-    bool IsOverlappingSolid_Or_SemiSolid(CellMap* _map, olc::Decal *_decal, std::string _layer, olc::vf2d _position);
+    bool IsOverlappingSolid_Or_SemiSolid(Level* _map, olc::Decal *_decal, std::string _layer, olc::vf2d _position);
 
     virtual void AdjustCollisionX(Level* _map);
     virtual void AdjustCollisionY(Level* _map);
