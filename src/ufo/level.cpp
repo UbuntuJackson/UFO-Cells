0cc199bb1b19d5aa360e3d50f20a5925d45e2ad0
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-12-14 13:24:25 +0100

    removed the ReadLevelFromFile call in level.cpp

--- src/ufo/level.cpp
+++ src/ufo/level.cpp
@@ -13,9 +13,7 @@
 #include "../../src/ufo/file_utils.h"
 
 Level::Level(std::string _path) :
-    loading_progress{0}{
-        ReadLevelFromFile(_path);
-    }
+    loading_progress{0}{}
 
 Layer*
 Level::NewLayer(std::string _name, std::string _type, std::string _path){
