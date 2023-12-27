3f4f7d30656c62f59edfef617179bb2442f24765
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-12-27 21:18:57 +0100

    slope adjustment should not happen on the mere condition that collision occurs initially

--- src/ufo/cell_actor.cpp
+++ src/ufo/cell_actor.cpp
@@ -363,6 +363,7 @@ CellActor::ApplyUpSlope(Level* _map){
                     temporary_slope_adjustment_position.y -= 1.0f;
                 }
             }
+            if(IsOverlapping(_map,mask_decal, solid_layer, temporary_slope_adjustment_position)) position.y = temporary_slope_adjustment_position.y;
         }
 
         if(velocity.x < 0.0f){
@@ -383,8 +384,8 @@ CellActor::ApplyUpSlope(Level* _map){
                     temporary_slope_adjustment_position.y -= 1.0f;
                 }
             }
+            if(IsOverlapping(_map,mask_decal, solid_layer, temporary_slope_adjustment_position)) position.y = temporary_slope_adjustment_position.y;
         }
-        position.y = temporary_slope_adjustment_position.y;
     }
     
 }
