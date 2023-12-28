47fd9370afac4ce5f494471276173bdf1b24146d
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-12-28 16:53:51 +0100

    a

--- src/ufo/cell_actor.cpp
+++ src/ufo/cell_actor.cpp
@@ -502,16 +502,16 @@ CellActor::ApplyDownSlope_SemiSolid(CellMap* _map){
 void
 CellActor::CB_AdjustDownSlope(Level* _lvl){
     int down_slope_range = 8;
-    if(!IsOverlapping(_map, mask_decal, solid_layer, olc::vf2d(position.x, position.y + 1.0f), olc::WHITE)){
+    if(!IsOverlapping(_lvl, mask_decal, solid_layer, olc::vf2d(position.x, position.y + 1.0f), olc::WHITE)){
         for(int i= 0; i < down_slope_range; i++){
             olc::vf2d before_down_slope_adj = position;
-            if(!IsOverlapping(_map, mask_decal, solid_layer, olc::vf2d(position.x, position.y + 1.0f), olc::WHITE)){
+            if(!IsOverlapping(_lvl, mask_decal, solid_layer, olc::vf2d(position.x, position.y + 1.0f), olc::WHITE)){
                 position.y += 1.0f;
             }
             else{
                 break;
             }
-            if(i == down_slope_range-1 && IsOverlapping(_map, mask_decal, solid_layer, position, olc::WHITE)){
+            if(i == down_slope_range-1 && IsOverlapping(_lvl, mask_decal, solid_layer, position, olc::WHITE)){
                 position = before_down_slope_adj;
             }
         }
