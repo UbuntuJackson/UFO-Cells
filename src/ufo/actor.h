42ef92fe0fca5f5ab1d22ea9fc240383a2ab920c
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-01-20 12:36:36 +0100

    a

--- src/ufo/actor.h
+++ src/ufo/actor.h
@@ -9,13 +9,13 @@
 class Camera;
 class Actor{
 public:
-    std::function<void(int)> remove_pointer_from_container = [](int){std::cout << "Destruct function empty.";};
+    std::function<void(int)>* remove_pointer_from_container = [](int){std::cout << "Destruct function empty.";};
     std::vector<Actor> attachments;
     int id;
     std::string layer_tag;
     olc::vf2d position;
     Actor(int _id, olc::vf2d _position, std::string _layer_tag);
-    Actor(int _id, olc::vf2d _position, std::string _layer_tag, std::function<void(int)> _destruct_instance);
+    Actor(int _id, olc::vf2d _position, std::string _layer_tag, std::function<void(int)> *_destruct_instance);
     virtual void Update();
     virtual void Draw(Camera* _camera);
     virtual void Draw();
--- src/ufo/container.h
+++ src/ufo/container.h
@@ -27,9 +27,13 @@ public:
     }
     template<typename ...Args>
     T* New(Args ...args){
-        std::unique_ptr<T> instance = std::make_unique<T>(args ...);
-        container.push_back(instance.get());
-        level->actors.push_back(std::move(instance));
+        //std::unique_ptr<T> instance = std::make_unique<T>(args ...);
+        //container.push_back(instance.get());
+        //level->actors.push_back(std::move(instance));
+        T* act = new T(args ...)
+        act->remove_pointer_from_container = &remove_ptr;
+        container.push_back(act);
+        level->actors.push_back(act);
     }
 };
 
