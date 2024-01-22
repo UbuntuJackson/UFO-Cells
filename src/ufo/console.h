d32345b15f1ad29ac7d72dc6e86ff528660633c4
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-01-22 23:53:04 +0100

    added console class for printing messges

---
+++ src/ufo/console.h
@@ -0,0 +1,24 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+#include <iostream>
+#include <vector>
+#include <string>
+class Console{
+public:
+  template<typename Arg, typename ...Args>
+  static void Out(Arg arg, Args ...args){
+    std::cout << arg;
+    ((std::cout << ", " << args), ...) << std::endl;
+  }
+  
+  template<typename T>
+  static void Out(std::vector<T> _vec){
+    for(int i = 0; i < _vec.size(); i++){
+        if(i != 0) std::cout << ", " << _vec[i];
+        else std::cout << _vec[i];
+    }
+    std::cout << std::endl;
+  }
+};
+
+#endif
