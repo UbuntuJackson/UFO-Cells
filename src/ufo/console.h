95f327b244b09836095098b20037b5e57dac11fe
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-01-23 00:10:32 +0100

    printing an endl after Out

--- src/ufo/console.h
+++ src/ufo/console.h
@@ -8,7 +8,8 @@ public:
   template<typename Arg, typename ...Args>
   static void Out(Arg&& arg, Args&& ...args){
     std::cout << arg;
-    ((std::cout << ", " << args), ...) << std::endl;
+    ((std::cout << ", " << args), ...)
+    std::cout << std::endl;
   }
   
   template<typename T>
