7ced2e73e77928dc8ede676df7df4cff4a4658cc
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-01-25 11:42:58 +0100

    a

--- src/ufo/fundamental.h
+++ src/ufo/fundamental.h
@@ -13,7 +13,7 @@ public:
         T *node = new T(args ...);
         node->parent = this;
         nodes.push_back(node);
-        return dynamic_cast<T>(*(nodes.back()));
+        return *nodes.back();
     }
     virtual void Update(){
         Console::Out("hello from Fundamental");
