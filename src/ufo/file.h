20374dcf95792f4f0c2829f4260755008786d401
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-01-30 18:06:06 +0100

    accidental first argument

--- src/ufo/file.h
+++ src/ufo/file.h
@@ -16,7 +16,7 @@ public:
     void Write(std::string _filename, std::string _text);
     
     template<typename Arg, typename ...Args>
-    void Insert(Arg&& arg, Args&& ...args){
+    void Insert(Args&& ...args){
         ((contents << args), ...);
     }
 };
