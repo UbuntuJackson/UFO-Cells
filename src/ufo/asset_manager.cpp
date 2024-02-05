af95acd69b5680e3ba34950ba184d8f51419eff2
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-02-05 20:52:15 +0100

    asset_manager added as well as remove decal and remove sprite functions

---
+++ src/ufo/asset_manager.cpp
@@ -0,0 +1,64 @@
+#include "../../olcPixelGameEngine.h"
+#include <memory>
+#include "asset_manager.h"
+#include "console.h"
+
+void AssetManager::LoadSprite(const std::string& _path, const std::string& _name) {
+    olc::Sprite* spr = new olc::Sprite();
+
+    switch (spr->LoadFromFile(_path)){
+        case olc::rcode::NO_FILE:
+            Console::Out("Error: The file does not exist at path", _path);
+            break;
+        case olc::rcode::FAIL:
+            Console::Out("Error: Failed to load the file at path", _path);
+            break;
+        default:
+            break;
+    }
+
+    sprites[_name] = std::make_unique<olc::Sprite>(spr);
+}
+
+olc::Sprite*
+AssetManager::GetSprite(std::string _name) {
+    if(sprites.count(_name)) Console::Out("Error: No entry with given key.");
+    return sprites.at(_name).get();
+}
+
+void
+AssetManager::RemoveSprite(){
+    if(!sprites.erase(_name)) Console::Out("Error: Sprite with given key does not exist.");
+}
+
+void AssetManager::LoadDecal(const std::string& _path, const std::string& _name) {
+    olc::Sprite* spr = new olc::Sprite();
+
+    switch (spr->LoadFromFile(_path)){
+        case olc::rcode::NO_FILE:
+            Console::Out("Error: The file does not exist at path", _path);
+            break;
+        case olc::rcode::FAIL:
+            Console::Out("Error: Failed to load the file at path", _path);
+            break;
+        default:
+            break;
+    }
+
+    olc::Decal* dec = new olc::Decal(spr);
+
+    sprites[_name] = std::make_unique<olc::Sprite>(spr);
+    decals[_name] = std::make_unique<olc::Decal>(dec);
+}
+
+olc::Decal*
+AssetManager::GetDecal(std::string _name) {
+    if(decals.count(_name)) Console::Out("Error: No entry with given key.");
+    return decals.at(_name).get();
+}
+
+void
+AssetManager::RemoveDecal(){
+    if(!sprites.erase(_name)) Console::Out("Error: Sprite with given key does not exist.");
+    if(!decals.erase(_name)) Console::Out("Error: Sprite with given key does not exist.");
+}
---
+++ src/ufo/asset_manager.h
@@ -0,0 +1,22 @@
+#ifndef ASSET_MANAGER_H
+#define ASSET_MANAGER_H
+#include "../../external/olcPixelGameEngine.h"
+#include <string>
+#include <map>
+#include <memory>
+
+class AssetManager{
+
+    std::map<std::string, std::unique_ptr<olc::Sprite>> sprites;
+    std::map<std::string,std::unique_ptr<olc::Decal>> decals;
+
+    void LoadSprite(const std::string& _path, const std::string& _name);
+    olc::Sprite* GetSprite(std::string _name);
+    void LoadDecal(const std::string& _path, const std::string& _name); //wouldn't this load both a sprite and a decal? And should the load functions return something?
+    olc::Decal* GetDecal(std::string _name);
+
+    AssetManager& GetSelf(){static AssetManager self; return self;} //this practically makes AssetManager global.
+
+};
+
+#endif
