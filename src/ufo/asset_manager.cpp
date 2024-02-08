09a4492677eaa0149a9f548b96475086c18006e3
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-02-08 12:30:58 +0100

    trying to return self.sprites

--- src/ufo/asset_manager.cpp
+++ src/ufo/asset_manager.cpp
@@ -2,9 +2,11 @@
 #include <memory>
 #include "asset_manager.h"
 #include "console.h"
+#include <map>
 
 std::map<std::string, std::unique_ptr<olc::Sprite>>& AssetManager::GetSprites(){
-    return sprites;
+    static AssetManager self;
+    return self.sprites;
 }
 
 void AssetManager::LoadSprite(const std::string& _path, const std::string& _name) {
