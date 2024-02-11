8af24534444179b24485d740586786916c9f208d
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-02-11 21:43:07 +0100

    forgot to make LayerProcess member of Actor

--- src/ufo/actor.cpp
+++ src/ufo/actor.cpp
@@ -2,6 +2,7 @@
 #include "camera.h"
 #include "../../external/olcPixelGameEngine.h"
 #include <string>
+#include "layer.h"
 
 Actor::Actor(int _id, olc::vf2d _position, std::string _layer_tag) : id{_id}, position{_position}, layer_tag{_layer_tag}{}
 
@@ -14,7 +15,7 @@ void
 Actor::Update(){}
 
 void
-LayerProcess(Layer* _layer){
+Actor::LayerProcess(Layer* _layer){
     
 }
 
