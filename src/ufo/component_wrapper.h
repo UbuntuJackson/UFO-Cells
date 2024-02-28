b6398659c62824c56fff2f0eb9b92dac8e34fc52
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2024-02-28 14:42:38 +0100

    updated component_wrapper.cpp

--- src/ufo/component_wrapper.h
+++ src/ufo/component_wrapper.h
@@ -1,13 +1,16 @@
 #ifndef COMPONENT_WRAPPER_H
 #define COMPONENT_WRAPPER_H
 #include "component.h"
+#include "scene.h"
 
 template<class tType>
 class ComponentWrapper : public Component{
 public:
     tType component;
     template<typename ...Args>
-    ComponentWrapper(Args ..._args) : component{tType(_args...)}{}
+    ComponentWrapper(Args ..._args) : component{tType(_args...)}{
+        scene_ptr->NewActor<tType>(_args...);
+    }
     tType& GetComponent(){return component;}
 };
 
